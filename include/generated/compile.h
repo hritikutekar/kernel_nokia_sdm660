92c84063107e19655f214fcc37e8544705a65c85
hritik <hritikdrocks@gmail.com>
2019-02-25 18:38:56 +0000

    from nokia

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP PREEMPT Mon Feb 25 17:58:11 UTC 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "instance-1"
+#define LINUX_COMPILER "gcc version 7.3.0 (Ubuntu 7.3.0-27ubuntu1~18.04) "
